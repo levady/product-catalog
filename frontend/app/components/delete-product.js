ab8fc1390251607fd937260a07768864e5c1177f
levady <levady@tradegecko.com>
2017-03-19 23:04:35 +0800

    Add delete product functionality.

---
+++ frontend/app/components/delete-product.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    deleteProduct() {
+      if (confirm('Are you sure you want to delete this product?')) {
+        this.get('product').destroyRecord().then(() => {
+          this.get('controller').transitionToRoute('products');
+        },
+
+        (error) => {
+          // TODO: handle error
+        });
+      }
+    },
+  },
+});
