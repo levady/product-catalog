3afc9ebb3675cff8a9111850d7b402e520f4cadc
levady <levady@tradegecko.com>
2017-03-19 15:55:44 +0800

    Add product show endpoint.

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::ProductsController < Api::V1::BaseController
   def index
     respond_with Product.all
   end
+
+  def show
+    respond_with Product.find(params[:id])
+  end
 end
--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -15,4 +15,23 @@ RSpec.describe Api::V1::ProductsController, type: :controller do
       expect(json.map { |p| p['name'] }).to contain_exactly('Product 1', 'Product 2')
     end
   end
+
+  describe "GET #show" do
+    let(:product) { create(:product, name: 'Product 1') }
+
+    context "product exists" do
+      it 'returns a product' do
+        get :show, id: product.id, format: :json
+        expect(response).to have_http_status(:success)
+        json = JSON.parse(response.body)
+        expect(json['name']).to eq('Product 1')
+      end
+    end
+
+    context "product does not exists" do
+      it "raise active record not found" do
+        expect { get :show, id: 10000, format: :json }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 end
