03a68bd29d6acef657bb367edd70a9d0391c2308
levady <levady@tradegecko.com>
2017-03-27 11:29:04 +0800

    Stripe whitespace from name and description.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,13 @@ class Product < ActiveRecord::Base
   validates :name, :price, presence: true
   validates :name, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+  before_validation :strip_whitespace
+
+private
+
+  def strip_whitespace
+    self.name&.strip!
+    self.description&.strip!
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_uniqueness_of(:name) }
   it { is_expected.to validate_numericality_of(:price) }
   it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
+
+  it "stripe whitespace from name and description" do
+    subject.name = '   strip whitespace please     '
+    subject.description = '    description     '
+    subject.save
+    expect(subject.name).to eq('strip whitespace please')
+    expect(subject.description).to eq('description')
+  end
 end
